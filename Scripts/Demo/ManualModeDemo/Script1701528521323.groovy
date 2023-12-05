04556ec78457ab691f878d3f9e7beddd35944e88
ngan30497 <ntkngan30497>
2023-12-05 23:26:08 +0700

    Update username

--- Scripts/Demo/ManualModeDemo/Script1701528521323.groovy
+++ Scripts/Demo/ManualModeDemo/Script1701528521323.groovy
@@ -21,7 +21,7 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-WebUI.setText(findTestObject('Page_OrangeHRM/input_Username_username'), 'Admin1')
+WebUI.setText(findTestObject('Page_OrangeHRM/input_Username_username'), 'Admin_update')
 
 WebUI.setEncryptedText(findTestObject('Page_OrangeHRM/input_Password_password'), 'hUKwJTbofgPU9eVlw/CnDQ==')
 
