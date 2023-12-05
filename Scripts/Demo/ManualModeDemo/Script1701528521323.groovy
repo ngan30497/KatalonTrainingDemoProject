d2c24b5215a6f7d56c072cbfb8ea624e51793265
ngan30497 <ntkngan30497>
2023-12-05 22:38:43 +0700

    g

--- Scripts/Demo/ManualModeDemo/Script1701528521323.groovy
+++ Scripts/Demo/ManualModeDemo/Script1701528521323.groovy
@@ -21,7 +21,7 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-WebUI.setText(findTestObject('Page_OrangeHRM/input_Username_username'), 'Admin')
+WebUI.setText(findTestObject('Page_OrangeHRM/input_Username_username'), 'Admin1')
 
 WebUI.setEncryptedText(findTestObject('Page_OrangeHRM/input_Password_password'), 'hUKwJTbofgPU9eVlw/CnDQ==')
 
